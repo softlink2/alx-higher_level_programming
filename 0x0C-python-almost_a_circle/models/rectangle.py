a3b9841addeaca34718f87b7fe1a9b0301b67e40
softlink2 <121310192+softlink2@users.noreply.github.com>
2023-04-22 11:16:49 +0400

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,58 +1,57 @@
 #!/usr/bin/python3
-"""Class "Rectangle" inherits fron "Base" class.
-With private instance attributes, each with its
-own public getter and setter"""
+"""Module for Rectangle class"""
+
 from models.base import Base
 
 
 class Rectangle(Base):
+    """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes the Rectangle instances
-        Args:
-            id(int):The Rectangle's identity
-            width(int): Rectangle's width
-            height(int): Rectangle's height
-            x(int): The x coordinate of the rectangle
-            y(int): The y coordinate of the rectangle
-        """
+        """Initialize Rectangle instance"""
+
+        super().__init__(id)
         self.width = width
-        self.eight = height
+        self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
-        @property
-        def width(self):
-            """set/get the rectangle's width"""
-            return self.__width
+    @property
+    def width(self):
+        """Getter for width attribute"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute"""
+        self.__width = value
 
-        @property
-        def height(self):
-            """set/get the rectangle's height"""
-            return self.__height
+    @property
+    def height(self):
+        """Getter for height attribute"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Setter for height attribute"""
+        self.__height = value
 
-        @property
-        def x(self):
-            """set/get the rectangle's 'x' coordinate"""
-            return self.__x
+    @property
+    def x(self):
+        """Getter for x attribute"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """Setter for x attribute"""
+        self.__x = value
 
-        @property
-        def y(self):
-            """set/get the rectangle's 'y' coordinate"""
-            return self.__y
+    @property
+    def y(self):
+        """Getter for y attribute"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """Setter for y attribute"""
+        self.__y = value
